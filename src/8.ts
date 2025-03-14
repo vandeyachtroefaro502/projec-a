96cdbc4e4517041a9c616adad4740ed4064c10c7
vandeyachtroefaro502 <vandeyachtroefaro502@gmail.com>
2025-03-14 06:30:33 +0000

    Add 8.ts

---
+++ src/8.ts
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function Example() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>Increment</button>
+    </div>
+  );
+}
