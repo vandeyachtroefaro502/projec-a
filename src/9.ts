27c9246304596e90b542dc778522ef3f51048e09
vandeyachtroefaro502 <vandeyachtroefaro502@gmail.com>
2025-03-15 23:18:27 +0000

    Add 9.ts

---
+++ src/9.ts
@@ -0,0 +1,9 @@
+
+const projectA = {
+  name: 'Projec-a',
+  description: 'A school project focused on using Git and GitHub for version control',
+  author: 'Your Name',
+  date: new Date()
+}
+
+module.exports = projectA
